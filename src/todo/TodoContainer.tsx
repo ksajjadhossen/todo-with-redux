9331f052a188c1c8eda063460cbdb44badf3da5f
ksajjadhossen <sk.sajjad4207@gmail.com>
2025-01-28 21:48:55 +0600

    dropdown added on todo  project

--- src/todo/TodoContainer.tsx
+++ src/todo/TodoContainer.tsx
@@ -1,14 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { FC } from "react";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer: FC = () => {
 	return (
-		<div>
-			<div>
-				<button>Add todo</button>
-				<button>Filter</button>
+		<div className="w-2/3 mx-auto">
+			<div className="flex justify-between p-5">
+				<AddTodoModal></AddTodoModal>
+				<Button>Filter</Button>
 			</div>
-			<div className="bg-gray-700 text-white rounded-lg w-2/3 mx-auto p-3">
+			<div className="bg-gray-700 text-white rounded-lg  p-3">
 				{/* <div className="text-center font-bold text-2xl p-2">
 					There is no task pending
 				</div> */}
---
+++ src/todo/todoFilter.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const todoFilter: FC = () => {
+	return <div>todoFilter</div>;
+};
+
+export default todoFilter;
