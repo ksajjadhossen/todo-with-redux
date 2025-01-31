f9b1ad355790815a272571f0dc6310fc2cd1fd2b
ksajjadhossen <sk.sajjad4207@gmail.com>
2025-01-31 22:52:13 +0600

    added id and isCompleted data automated

--- src/redux/features/todo.slice.ts
+++ src/redux/features/todo.slice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 type TTodo = {
+	id?: string;
 	tittle: string;
 	description: string;
 	isCompleted?: boolean;
@@ -16,7 +17,11 @@ const todoSlice = createSlice({
 	initialState,
 	reducers: {
 		addTodo: (state, action: PayloadAction<TTodo>) => {
-			state.todos.push({ ...action.payload, isCompleted: false });
+			state.todos.push({
+				...action.payload,
+				isCompleted: false,
+				id: Math.random().toString(36).substr(2, 9),
+			});
 		},
 	},
 });
--- src/redux/features/todo.store.ts
+++ src/redux/features/todo.store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoSlice from "./todo.slice";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		todoSlice: todoSlice,
+	},
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
